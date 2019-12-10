fcd48fb5b2a1073f2c6c108c080592366eade2e7
surpreetmann <surpreetmann35@gmail.com>
2019-12-10 17:19:58 +0530

    files added

--- app.js
+++ app.js
@@ -24,15 +24,15 @@ app.engine('handlebars',exphbs({defaultLayout:'layout'}));
 app.set('view engine', 'handlebars');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // express session
 app.use(session({
   secret: 'ElearnBySurpreet',
-  saveuninitialized: true,
+  saveUninitialized: true,
   resave: true
 }));
 
