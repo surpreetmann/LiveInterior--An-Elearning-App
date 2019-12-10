51c73d941158b87205baff4bbc451655fc5e3092
surpreetmann <surpreetmann35@gmail.com>
2019-12-10 12:47:02 +0530

    files

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser =require('body-parser');
+var exphbs =require('express-handlebars');
+var expressValidator =require('express-validator');
+var flash =require('connect-flash');
+var session =require('express-session');
+var mongo =require('mongodb');
+var mongoose =require('mongoose');
+var passport =require('passport');
+var localStrategy =require('passport-local'),Strategy;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,13 +20,52 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars',exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// express session
+app.use(session({
+  secret: 'ElearnBySurpreet',
+  saveuninitialized: true,
+  resave: true
+}));
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// express validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value){
+    var namespace=param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length){
+      formparam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg : msg,
+      value : value
+    };
+  }
+}));
+
+// connect-flash
+app.use(flash());
+
+// global vars
+app.use(function(req,res,next){
+  res.locals.messages=require('express-messages')(req,res);
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
