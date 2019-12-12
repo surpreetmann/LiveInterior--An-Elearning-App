0ff8ada078357a3f9d50e2eab27dc09245734501
surpreetmann <surpreetmann35@gmail.com>
2019-12-12 19:35:01 +0530

    files

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//Register user
+router.get('/register', function(req, res, next) {
+  res.render('users/register');
 });
 
 module.exports = router;
