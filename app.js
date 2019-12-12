f15d1c5c976f1c27c2ec69034a06e117a0ea9149
surpreetmann <surpreetmann35@gmail.com>
2019-12-12 16:32:53 +0530

    added files

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var db=mongoose.connection;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var classesRouter = require('./routes/classes');
 
 var app = express();
 
@@ -71,6 +72,8 @@ app.use(function(req,res,next){
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/classes', classesRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
